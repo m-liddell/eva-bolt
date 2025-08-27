b3e7612533cefa76d2316b1e5da05f1b751c7084
m-liddell <36708674+m-liddell@users.noreply.github.com>
2025-08-27 07:09:38 +0100

    Fix CSS and Stylesheets for Next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '../src/index.css'
 import { ThemeProvider } from '../src/context/ThemeContext'
 
 const inter = Inter({ subsets: ['latin'] })
